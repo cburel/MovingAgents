2a43a4e2885bbfc728d243eca2999ca87421e573
Celeste Burel <cburel@uccs.edu>
2023-09-09 17:44:54 -0600

    added a half-broken wander behavior to enemy

--- MovingAgents/MovingAgents/Enemy.py
+++ MovingAgents/MovingAgents/Enemy.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame.locals import *
+import random
+import math
 import Constants
 
 class Enemy:
@@ -33,15 +35,28 @@ class Enemy:
 		pygame.draw.line(screen, (0, 0, 255), lineStart, lineEnd, 3)
 
 	def update(self, player):
-
+		
 		# flee if player is close enough
 		distance = self.pos - player.pos
 		if distance.length() < Constants.FLEE_RANGE:
-			self.vel = pygame.Vector2.normalize(distance)
-			#Use speed and multiply it by velocity and time in order to determine where the enemy should move
+			self.vel = pygame.Vector2.normalize(distance) * Constants.ENEMY_SPD
+			self.pos += self.vel
 
 		# otherwise, wander
-		print("Wander...")
+		else:
+			rotationAngle = random.randrange(-1, 1)
+			theta = math.acos(rotationAngle)
+
+			pickTurn = random.randint(0, 100)
+			if pickTurn < 50:
+				pass
+			else:
+				theta += 180
+
+			self.vel.x += math.cos(theta) - math.sin(theta)
+			self.vel.y += math.sin(theta) - math.cos(theta)
+
+			self.pos += pygame.Vector2.normalize(self.vel)
 
 	def calcCenter(self):
 		return pygame.Vector2(self.pos.x + self.size / 2, self.pos.y + self.size / 2)
--- MovingAgents/MovingAgents/Player.py
+++ MovingAgents/MovingAgents/Player.py
@@ -39,7 +39,9 @@ class Player:
 
 	def update(self, enemies: List):
 
-		enemy = enemies[0]
+		# gets nearest enemy and moves player towards it
+		# shoutout to Rabbid76 on SO for the basics on this next line
+		enemy = min([e for e in enemies], key=lambda e: self.pos.distance_to(pygame.math.Vector2(e.pos.x, e.pos.y)))
 		self.vel = enemy.pos - self.pos
 				
 		self.vel = pygame.Vector2.normalize(self.vel)
